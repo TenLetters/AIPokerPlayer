658c16754ad1ac30704731a05031259512ad9d8e
TenLetters <10letterz@gmail.com>
2015-10-24 14:06:10 -0400

    created UI folder

--- AIPokerPlayer/Program.cs
+++ AIPokerPlayer/Program.cs
@@ -16,7 +16,7 @@ namespace AIPokerPlayer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SettingsForm());
         }
     }
 }
