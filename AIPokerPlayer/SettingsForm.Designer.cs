a0fc21787700ab4f5c370fb1b7e2cbec12dc369b
TenLetters <10letterz@gmail.com>
2015-10-24 14:02:14 -0400

    Initial Commit. Created sample settings form layout

---
+++ AIPokerPlayer/SettingsForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace AIPokerPlayer
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPlayerOne = new System.Windows.Forms.Label();
+            this.labelPlayerThree = new System.Windows.Forms.Label();
+            this.labelPlayerTwo = new System.Windows.Forms.Label();
+            this.labelPlayerFour = new System.Windows.Forms.Label();
+            this.labelPlayerFive = new System.Windows.Forms.Label();
+            this.labelPlayerSix = new System.Windows.Forms.Label();
+            this.labelPlayerSeven = new System.Windows.Forms.Label();
+            this.labelPlayerEight = new System.Windows.Forms.Label();
+            this.comboBoxPlayerOne = new System.Windows.Forms.ComboBox();
+            this.comboBoxPlayerTwo = new System.Windows.Forms.ComboBox();
+            this.comboBoxPlayerThree = new System.Windows.Forms.ComboBox();
+            this.comboBoxPlayerFour = new System.Windows.Forms.ComboBox();
+            this.comboBoxPlayerSix = new System.Windows.Forms.ComboBox();
+            this.comboBoxPlayerFive = new System.Windows.Forms.ComboBox();
+            this.comboBoxPlayerEight = new System.Windows.Forms.ComboBox();
+            this.comboBoxPlayerSeven = new System.Windows.Forms.ComboBox();
+            this.buttonStartGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelPlayerOne
+            // 
+            this.labelPlayerOne.AutoSize = true;
+            this.labelPlayerOne.Location = new System.Drawing.Point(31, 63);
+            this.labelPlayerOne.Name = "labelPlayerOne";
+            this.labelPlayerOne.Size = new System.Drawing.Size(59, 13);
+            this.labelPlayerOne.TabIndex = 0;
+            this.labelPlayerOne.Text = "Player One";
+            // 
+            // labelPlayerThree
+            // 
+            this.labelPlayerThree.AutoSize = true;
+            this.labelPlayerThree.Location = new System.Drawing.Point(24, 117);
+            this.labelPlayerThree.Name = "labelPlayerThree";
+            this.labelPlayerThree.Size = new System.Drawing.Size(67, 13);
+            this.labelPlayerThree.TabIndex = 1;
+            this.labelPlayerThree.Text = "Player Three";
+            // 
+            // labelPlayerTwo
+            // 
+            this.labelPlayerTwo.AutoSize = true;
+            this.labelPlayerTwo.Location = new System.Drawing.Point(30, 90);
+            this.labelPlayerTwo.Name = "labelPlayerTwo";
+            this.labelPlayerTwo.Size = new System.Drawing.Size(60, 13);
+            this.labelPlayerTwo.TabIndex = 2;
+            this.labelPlayerTwo.Text = "Player Two";
+            // 
+            // labelPlayerFour
+            // 
+            this.labelPlayerFour.AutoSize = true;
+            this.labelPlayerFour.Location = new System.Drawing.Point(30, 144);
+            this.labelPlayerFour.Name = "labelPlayerFour";
+            this.labelPlayerFour.Size = new System.Drawing.Size(60, 13);
+            this.labelPlayerFour.TabIndex = 3;
+            this.labelPlayerFour.Text = "Player Four";
+            // 
+            // labelPlayerFive
+            // 
+            this.labelPlayerFive.AutoSize = true;
+            this.labelPlayerFive.Location = new System.Drawing.Point(31, 171);
+            this.labelPlayerFive.Name = "labelPlayerFive";
+            this.labelPlayerFive.Size = new System.Drawing.Size(59, 13);
+            this.labelPlayerFive.TabIndex = 4;
+            this.labelPlayerFive.Text = "Player Five";
+            // 
+            // labelPlayerSix
+            // 
+            this.labelPlayerSix.AutoSize = true;
+            this.labelPlayerSix.Location = new System.Drawing.Point(37, 198);
+            this.labelPlayerSix.Name = "labelPlayerSix";
+            this.labelPlayerSix.Size = new System.Drawing.Size(53, 13);
+            this.labelPlayerSix.TabIndex = 5;
+            this.labelPlayerSix.Text = "Player Six";
+            // 
+            // labelPlayerSeven
+            // 
+            this.labelPlayerSeven.AutoSize = true;
+            this.labelPlayerSeven.Location = new System.Drawing.Point(20, 225);
+            this.labelPlayerSeven.Name = "labelPlayerSeven";
+            this.labelPlayerSeven.Size = new System.Drawing.Size(70, 13);
+            this.labelPlayerSeven.TabIndex = 6;
+            this.labelPlayerSeven.Text = "Player Seven";
+            // 
+            // labelPlayerEight
+            // 
+            this.labelPlayerEight.AutoSize = true;
+            this.labelPlayerEight.Location = new System.Drawing.Point(27, 252);
+            this.labelPlayerEight.Name = "labelPlayerEight";
+            this.labelPlayerEight.Size = new System.Drawing.Size(63, 13);
+            this.labelPlayerEight.TabIndex = 7;
+            this.labelPlayerEight.Text = "Player Eight";
+            // 
+            // comboBoxPlayerOne
+            // 
+            this.comboBoxPlayerOne.FormattingEnabled = true;
+            this.comboBoxPlayerOne.Location = new System.Drawing.Point(96, 60);
+            this.comboBoxPlayerOne.Name = "comboBoxPlayerOne";
+            this.comboBoxPlayerOne.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPlayerOne.TabIndex = 8;
+            // 
+            // comboBoxPlayerTwo
+            // 
+            this.comboBoxPlayerTwo.FormattingEnabled = true;
+            this.comboBoxPlayerTwo.Location = new System.Drawing.Point(96, 87);
+            this.comboBoxPlayerTwo.Name = "comboBoxPlayerTwo";
+            this.comboBoxPlayerTwo.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPlayerTwo.TabIndex = 9;
+            // 
+            // comboBoxPlayerThree
+            // 
+            this.comboBoxPlayerThree.FormattingEnabled = true;
+            this.comboBoxPlayerThree.Location = new System.Drawing.Point(96, 114);
+            this.comboBoxPlayerThree.Name = "comboBoxPlayerThree";
+            this.comboBoxPlayerThree.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPlayerThree.TabIndex = 10;
+            // 
+            // comboBoxPlayerFour
+            // 
+            this.comboBoxPlayerFour.FormattingEnabled = true;
+            this.comboBoxPlayerFour.Location = new System.Drawing.Point(96, 141);
+            this.comboBoxPlayerFour.Name = "comboBoxPlayerFour";
+            this.comboBoxPlayerFour.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPlayerFour.TabIndex = 11;
+            // 
+            // comboBoxPlayerSix
+            // 
+            this.comboBoxPlayerSix.FormattingEnabled = true;
+            this.comboBoxPlayerSix.Location = new System.Drawing.Point(96, 195);
+            this.comboBoxPlayerSix.Name = "comboBoxPlayerSix";
+            this.comboBoxPlayerSix.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPlayerSix.TabIndex = 12;
+            // 
+            // comboBoxPlayerFive
+            // 
+            this.comboBoxPlayerFive.FormattingEnabled = true;
+            this.comboBoxPlayerFive.Location = new System.Drawing.Point(96, 168);
+            this.comboBoxPlayerFive.Name = "comboBoxPlayerFive";
+            this.comboBoxPlayerFive.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPlayerFive.TabIndex = 13;
+            // 
+            // comboBoxPlayerEight
+            // 
+            this.comboBoxPlayerEight.FormattingEnabled = true;
+            this.comboBoxPlayerEight.Location = new System.Drawing.Point(96, 249);
+            this.comboBoxPlayerEight.Name = "comboBoxPlayerEight";
+            this.comboBoxPlayerEight.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPlayerEight.TabIndex = 14;
+            // 
+            // comboBoxPlayerSeven
+            // 
+            this.comboBoxPlayerSeven.FormattingEnabled = true;
+            this.comboBoxPlayerSeven.Location = new System.Drawing.Point(96, 222);
+            this.comboBoxPlayerSeven.Name = "comboBoxPlayerSeven";
+            this.comboBoxPlayerSeven.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPlayerSeven.TabIndex = 15;
+            // 
+            // buttonStartGame
+            // 
+            this.buttonStartGame.Location = new System.Drawing.Point(638, 566);
+            this.buttonStartGame.Name = "buttonStartGame";
+            this.buttonStartGame.Size = new System.Drawing.Size(158, 23);
+            this.buttonStartGame.TabIndex = 16;
+            this.buttonStartGame.Text = "Start Game";
+            this.buttonStartGame.UseVisualStyleBackColor = true;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 611);
+            this.Controls.Add(this.buttonStartGame);
+            this.Controls.Add(this.comboBoxPlayerSeven);
+            this.Controls.Add(this.comboBoxPlayerEight);
+            this.Controls.Add(this.comboBoxPlayerFive);
+            this.Controls.Add(this.comboBoxPlayerSix);
+            this.Controls.Add(this.comboBoxPlayerFour);
+            this.Controls.Add(this.comboBoxPlayerThree);
+            this.Controls.Add(this.comboBoxPlayerTwo);
+            this.Controls.Add(this.comboBoxPlayerOne);
+            this.Controls.Add(this.labelPlayerEight);
+            this.Controls.Add(this.labelPlayerSeven);
+            this.Controls.Add(this.labelPlayerSix);
+            this.Controls.Add(this.labelPlayerFive);
+            this.Controls.Add(this.labelPlayerFour);
+            this.Controls.Add(this.labelPlayerTwo);
+            this.Controls.Add(this.labelPlayerThree);
+            this.Controls.Add(this.labelPlayerOne);
+            this.Name = "SettingsForm";
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.SettingsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPlayerOne;
+        private System.Windows.Forms.Label labelPlayerThree;
+        private System.Windows.Forms.Label labelPlayerTwo;
+        private System.Windows.Forms.Label labelPlayerFour;
+        private System.Windows.Forms.Label labelPlayerFive;
+        private System.Windows.Forms.Label labelPlayerSix;
+        private System.Windows.Forms.Label labelPlayerSeven;
+        private System.Windows.Forms.Label labelPlayerEight;
+        private System.Windows.Forms.ComboBox comboBoxPlayerOne;
+        private System.Windows.Forms.ComboBox comboBoxPlayerTwo;
+        private System.Windows.Forms.ComboBox comboBoxPlayerThree;
+        private System.Windows.Forms.ComboBox comboBoxPlayerFour;
+        private System.Windows.Forms.ComboBox comboBoxPlayerSix;
+        private System.Windows.Forms.ComboBox comboBoxPlayerFive;
+        private System.Windows.Forms.ComboBox comboBoxPlayerEight;
+        private System.Windows.Forms.ComboBox comboBoxPlayerSeven;
+        private System.Windows.Forms.Button buttonStartGame;
+    }
+}
+
