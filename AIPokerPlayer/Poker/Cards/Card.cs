0dcd0a1cd2b8261ccae75ab5ad98063741071e6c
middle59 <callmemike94@aol.com>
2015-11-29 17:24:48 -0500

    Cards.cs updated. Resources/PNG-deck-cards/ added.

--- AIPokerPlayer/Poker/Cards/Card.cs
+++ AIPokerPlayer/Poker/Cards/Card.cs
@@ -1,24 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace AIPokerPlayer.Poker.Cards
 {
+    // enum of card value
+    public enum Value { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
+    // enum of card suit
+    public enum Suit { Clubs, Spades, Hearts, Diamonds };
+
     public class Card
-    {
-        public Card()
+    {   
+        //For reference. For embedding resources, we need the app-path
+        String path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        // image location relative to our bin/debug/.exe
+        String imageLocation = "../../Resources/PNG-deck-cards/";
+        Image myImage;
+        Value value;
+        Suit suit;
+
+        public String getPath() { return path; }
+        public Image getImage() { return myImage; }
+        
+
+        public Card(Value value, Suit suit)
         {
-
+            this.value = value;
+            this.suit = suit;
+            imageLocation += value + "_of_" + suit + ".png";
+            myImage = Image.FromFile(imageLocation);
         }
-        // enum of card value
-
-        // enum of card suit
-
-        // image string
-        String imageLocation;
-
     }
 
 }
---
+++ AIPokerPlayer/Resources/PNG-deck-cards/README.txt
@@ -0,0 +1,3 @@
+Attribution Not Required
+For more information see: 
+Byron Knoll: http://code.google.com/p/vector-playing-cards/
