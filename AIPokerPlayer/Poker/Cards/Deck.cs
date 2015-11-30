91b40cf46f7fd78d04e9536bf3eabe9c6eae0d3a
TenLetters <10letterz@gmail.com>
2015-11-30 13:39:13 -0500

    added in shuffle method to deck

--- AIPokerPlayer/Poker/Cards/Deck.cs
+++ AIPokerPlayer/Poker/Cards/Deck.cs
@@ -16,21 +16,99 @@ namespace AIPokerPlayer.Poker.Cards
         // the state of the game (time for turn, river, or flop)
         int stateOfGame;
 
+        Random random = new Random();
+
         public Deck()
         {
-            deck = new List<Card>();
             // put all of the cards into the deck in order
+            createNewUnsortedDeck();
             shuffle();
             stateOfGame = 0;
+        }
+        
+        public void createNewUnsortedDeck()
+        {
+            deck = new List<Card>();
+
+            // add all 4 twos
+            deck.Add(new Card(Value.Two, Suit.Clubs));
+            deck.Add(new Card(Value.Two, Suit.Diamonds));
+            deck.Add(new Card(Value.Two, Suit.Hearts));
+            deck.Add(new Card(Value.Two, Suit.Spades));
+            // add all 4 threes
+            deck.Add(new Card(Value.Three, Suit.Clubs));
+            deck.Add(new Card(Value.Three, Suit.Diamonds));
+            deck.Add(new Card(Value.Three, Suit.Hearts));
+            deck.Add(new Card(Value.Three, Suit.Spades));
+            // add all 4 fours
+            deck.Add(new Card(Value.Four, Suit.Clubs));
+            deck.Add(new Card(Value.Four, Suit.Diamonds));
+            deck.Add(new Card(Value.Four, Suit.Hearts));
+            deck.Add(new Card(Value.Four, Suit.Spades));
+            // add all 4 fives
+            deck.Add(new Card(Value.Five, Suit.Clubs));
+            deck.Add(new Card(Value.Five, Suit.Diamonds));
+            deck.Add(new Card(Value.Five, Suit.Hearts));
+            deck.Add(new Card(Value.Five, Suit.Spades));
+            // add all 4 sixes
+            deck.Add(new Card(Value.Six, Suit.Clubs));
+            deck.Add(new Card(Value.Six, Suit.Diamonds));
+            deck.Add(new Card(Value.Six, Suit.Hearts));
+            deck.Add(new Card(Value.Six, Suit.Spades));
+            // add all 4 sevens
+            deck.Add(new Card(Value.Seven, Suit.Clubs));
+            deck.Add(new Card(Value.Seven, Suit.Diamonds));
+            deck.Add(new Card(Value.Seven, Suit.Hearts));
+            deck.Add(new Card(Value.Seven, Suit.Spades));
+            // add all 4 eights
+            deck.Add(new Card(Value.Eight, Suit.Clubs));
+            deck.Add(new Card(Value.Eight, Suit.Diamonds));
+            deck.Add(new Card(Value.Eight, Suit.Hearts));
+            deck.Add(new Card(Value.Eight, Suit.Spades));
+            // add all 4 nines
+            deck.Add(new Card(Value.Nine, Suit.Clubs));
+            deck.Add(new Card(Value.Nine, Suit.Diamonds));
+            deck.Add(new Card(Value.Nine, Suit.Hearts));
+            deck.Add(new Card(Value.Nine, Suit.Spades));
+            // add all 4 tens
+            deck.Add(new Card(Value.Ten, Suit.Clubs));
+            deck.Add(new Card(Value.Ten, Suit.Diamonds));
+            deck.Add(new Card(Value.Ten, Suit.Hearts));
+            deck.Add(new Card(Value.Ten, Suit.Spades));
+            // add all 4 jacks
+            deck.Add(new Card(Value.Jack, Suit.Clubs));
+            deck.Add(new Card(Value.Jack, Suit.Diamonds));
+            deck.Add(new Card(Value.Jack, Suit.Hearts));
+            deck.Add(new Card(Value.Jack, Suit.Spades));
+            // add all 4 queens
+            deck.Add(new Card(Value.Queen, Suit.Clubs));
+            deck.Add(new Card(Value.Queen, Suit.Diamonds));
+            deck.Add(new Card(Value.Queen, Suit.Hearts));
+            deck.Add(new Card(Value.Queen, Suit.Spades));
+            // add all 4 kings
+            deck.Add(new Card(Value.King, Suit.Clubs));
+            deck.Add(new Card(Value.King, Suit.Diamonds));
+            deck.Add(new Card(Value.King, Suit.Hearts));
+            deck.Add(new Card(Value.King, Suit.Spades));
+            // add all 4 aces
+            deck.Add(new Card(Value.Ace, Suit.Clubs));
+            deck.Add(new Card(Value.Ace, Suit.Diamonds));
+            deck.Add(new Card(Value.Ace, Suit.Hearts));
+            deck.Add(new Card(Value.Ace, Suit.Spades));
         }
 
         // shuffle the deck of cards using the Fisher-Yates Shuffle method
         // reset the currentIndex to 0
         private void shuffle()
         {
-            for(int i = 0; i < 52; i++)
+            int n = deck.Count;
+
+            for(int i = 0; i < n; i++)
             {
-                deck.Add(new Card(Value.Ace, Suit.Spades));
+                int r = i + (int)(random.NextDouble() * (n - i));
+                Card card = deck[r];
+                deck[r] = deck[i];
+                deck[i] = card;
             }
         }
 
@@ -40,7 +118,7 @@ namespace AIPokerPlayer.Poker.Cards
             List<Card> result = new List<Card>();
 
             // add the cards to our result
-            for (int i = 0; i < numberOfCards; i++)
+            for (int i = currentIndex; i < numberOfCards + currentIndex; i++)
             {
                 result.Add(deck[i]);
             }
